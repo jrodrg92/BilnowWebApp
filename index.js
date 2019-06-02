7fb9a0905219edb77679d54a72d11d801b5fd381
Javier Rodríguez González <jrodrg92@gmail.com>
2019-06-02 22:54:33 +0200

    new index

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var init = require('./backend/src/routes');
+var init = require('./src/routes');
 var bodyParser=require('body-parser');
-var ejs = require('ejs');
 
 const app= express();
 
 // settings
 app.set('port', 3000);
+app.use('/views', express.static(__dirname + '/src/backend/views'));
 app.use('/frontend', express.static(__dirname + '/frontend'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
   extended:true
 }));
 
-init.main(app);
+app.use(init);
 
 // listening the Server
 app.listen(app.get('port'), () => {
