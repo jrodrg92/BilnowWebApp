7c2fdc29c8f072e50e4ad022dbb1b0b71fcca209
Javier Rodríguez González <jrodrg92@gmail.com>
2019-06-11 20:34:57 +0200

    modificando el modelo de usuario

--- src/backend/models/user.js
+++ src/backend/models/user.js
@@ -5,15 +5,16 @@ module.exports = {
     getUser: function(sequelize){
   
         const User= sequelize.define("usuarios",{
-            id_Usuario: {type: Sequelize.STRING, primaryKey: true},
-            nom_Usuario: Sequelize.STRING,
-            ap_Usuario: Sequelize.STRING,
-            tlf_Usuario: Sequelize.STRING,
-            dir_Usuario: Sequelize.STRING,
-            email_Usuario:Sequelize.STRING,
-            pswd_Usuario: Sequelize.STRING,
-            createdAt:Sequelize.TIME,
-            updatedAt:Sequelize.TIME
+                id_Usuario: {type: Sequelize.STRING, primaryKey: true},
+                nom_Usuario: Sequelize.STRING,
+                ap_Usuario: Sequelize.STRING,
+                tlf_Usuario: Sequelize.STRING,
+                dir_Usuario: Sequelize.STRING,
+                rol:Sequelize.INTEGER,
+                email_Usuario:Sequelize.STRING,
+                pswd_Usuario: Sequelize.STRING,
+                createdAt:Sequelize.TIME,
+                updatedAt:Sequelize.TIME
             },
             {  
                 freezeTableName: true 
