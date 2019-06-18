68fe3792ed3f0d7c1efded7f697f25bc36be5da5
Javier Rodríguez González <46450554+jrodrg92@users.noreply.github.com>
2019-06-18 16:07:26 +0200

    Update ctrlAddPetWin.js

--- src/backend/controllers/ctrlAddPetWin.js
+++ src/backend/controllers/ctrlAddPetWin.js
@@ -20,15 +20,37 @@ module.exports.showmodPet = function(req, res, pet){
 module.exports.modPet= function(req,res,petBD){
 
     var pet=getPet();
-    
-    petBD.update({ id_Mascota:pet.dataValues.id_Mascota,
-        nombre_Mascota:req.body.nombre_Mascota,
-        esp_Mascota:req.body.esp_Mascota,
-        raza_Mascota:req.body.raza_Mascota,
-        capa_Mascota:req.body.capa_Mascota,
-        fecha_Nacimiento:new Date(req.body.datepicker),
-        sexo_Mascota:req.body.sexo_Mascota,
-        id_Duenio:own.id_Usuario},{where:{id_Mascota:req.body.id_Mascota}}).then(
+    var nom, esp, raz, capa;
+    if(req.body.nombre_Mascota==null){
+        nom=pet.nombre_Mascota;
+    }
+    else{
+        nom=req.body.nombre_Mascota;
+    }
+    if(req.body.esp_Mascota==null){
+        esp=pet.esp_Mascota;
+    }
+    else{
+        esp=req.body.esp_Mascota;
+    }
+    if(req.body.raza_Mascota==null){
+        raz=pet.raza_Mascota;
+    }
+    else{
+        raz=req.body.raza_Mascota;
+    }
+    if(req.body.capa_Mascota==null){
+        capa=pet.capa_Mascota;
+    }
+    else{
+        capa=req.body.capa_Mascota;
+    }
+
+    petBD.update({nombre_Mascota:nom,
+        esp_Mascota:esp,
+        raza_Mascota:raz,
+        capa_Mascota:capa
+        },{where:{id_Mascota:pet.id_Mascota}}).then(
         pet=>{
             
         }
